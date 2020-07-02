c67e0ca93655de4c013395db28d665dfec541ea3
Deijah Wilson <deijahwilson1@gmail.com>
2020-07-02 13:00:02 -0400

    fixed join group button

--- guardian-backend-api/app/controllers/user_groups_controller.rb
+++ guardian-backend-api/app/controllers/user_groups_controller.rb
@@ -1,12 +1,21 @@
 class UserGroupsController < ApplicationController
     def index
         user_groups = UserGroup.all
-        render json: user_groups 
+        render json: user_groups.to_json(include: [:member, :group])
+    end
+
+    def show 
+        user_group = UserGroup.find(params[:id])
+        render json: user_group.to_json(include: [:member, :group])
     end
 
     def create 
         user_group = UserGroup.create(member_id: params[:member_id], group_id: params[:group_id])
-        render json: user_group
+        render json: user_group.to_json(include: [:member, :group])
+    end
+
+    def destroy
+        UserGroup.find(params[:id]).destroy
     end
 
 end
